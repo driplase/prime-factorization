e13442786bcb573890e944969a1a8351e0041e48
Driplase <258vr.g.subspeed@gmail.com>
2025-10-04 00:11:27 +0900

    feat: implement the base of the tool

---
+++ src/components/tool.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { ChangeEvent, useRef, useState } from "react";
+import { LoaderCircle } from 'lucide-react';
+
+export default function Tool() {
+  const [n, setN] = useState(2n);
+  const [result, setResult] = useState<[bigint, number][]>([]);
+  const [factorizationComplete, setFactorizationComplete] = useState<boolean>(true);
+  const cancelToken = useRef(0); // Add this line
+
+  function factorization(target: bigint) {
+    setResult([]);
+    setFactorizationComplete(false);
+
+    const nums: [bigint, number][] = [];
+    let k = target;
+    const myToken = ++cancelToken.current; // Increment token for this run
+
+    let i: bigint = 2n;
+    function step() {
+      // Cancel if token changed
+      if (cancelToken.current !== myToken) return;
+
+      const start = performance.now();
+      while (i * i <= k) {
+        let c = 0;
+
+        while (k % i === 0n) {
+          c++;
+          k /= i;
+
+          
+          setResult([...nums, [i, c]]);
+        }
+
+        if (c > 0) nums.push([i, c]);
+
+        i++;
+
+        if (performance.now() - start > 16) {
+          setTimeout(step, 0);
+          return;
+        }
+      }
+      
+      if (k > 1n) nums.push([k, 1]);
+
+      // Cancel if token changed before setting result
+      if (cancelToken.current !== myToken) return;
+      setResult(nums);
+      setFactorizationComplete(true)
+    }
+
+    step();
+  }
+
+  async function onTargetChange(e: ChangeEvent<HTMLInputElement>) {
+    setN(BigInt(e.target.value));
+    factorization(BigInt(e.target.value));
+  }
+
+  return (
+    <div className="bg-conic-315 from-indigo-100 via-fuchsia-100 to-indigo-100 px-6 py-4 rounded-xl border border-slate-500/72 ring ring-gray-400/64">
+      <h1 className="font-bold text-2xl mb-1 pb-1 text-center">Prime Factorization Tool</h1>
+
+      <label>
+        Target integer:
+        <input
+          className="ml-2 bg-white px-2 py-1 rounded-lg border border-slate-500 ring ring-slate-400/50"
+          type="number"
+          min={2} step={1}
+          onChange={onTargetChange}
+          placeholder="n ≧ 2"
+        />
+      </label>
+
+      <p className="mt-3">Result:</p>
+      <div>
+        { result.map(i => `${i[0].toString()}${i[1] > 1 ? ` ^ ${i[1]}` : ""}`).join(' * ') }
+        {!factorizationComplete && <LoaderCircle className="animate-spin" />}
+      </div>
+    </div>
+  );
+}

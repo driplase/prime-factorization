7dda0aabf26aef70b6688a56336c91d322117a6b
Driplase <258vr.g.subspeed@gmail.com>
2025-10-04 02:05:43 +0900

    chore: suppoer biggererer number

--- src/components/tool.tsx
+++ src/components/tool.tsx
@@ -4,9 +4,11 @@ import { ChangeEvent, useRef, useState } from "react";
 import { LoaderCircle } from 'lucide-react';
 
 export default function Tool() {
-  const [n, setN] = useState(2n);
+  const [n, setN] = useState<bigint | null>(null);
   const [result, setResult] = useState<[bigint, number][]>([]);
   const [factorizationComplete, setFactorizationComplete] = useState<boolean>(true);
+  const [dividerProgress, setDividerProgress] = useState<bigint | null>(null);
+  const [biggerNumber, setBiggerNumber] = useState<boolean>(false);
   const cancelToken = useRef(0); // Add this line
 
   function factorization(target: bigint) {
@@ -24,6 +26,7 @@ export default function Tool() {
 
       const start = performance.now();
       while (i * i <= k) {
+        setDividerProgress(i);
         let c = 0;
 
         while (k % i === 0n) {
@@ -63,18 +66,27 @@ export default function Tool() {
     <div className="bg-conic-315 from-indigo-100 via-fuchsia-100 to-indigo-100 px-6 py-4 rounded-xl border border-slate-500/72 ring ring-gray-400/64">
       <h1 className="font-bold text-2xl mb-1 pb-1 text-center">Prime Factorization Tool</h1>
 
+      <div>
+        <label>
+          Enable bigger number: <input type="checkbox" checked={biggerNumber} onChange={(e) => setBiggerNumber(e.target.checked)} />
+        </label>
+      </div>
+
       <label>
         Target integer:
         <input
           className="ml-2 bg-white px-2 py-1 rounded-lg border border-slate-500 ring ring-slate-400/50"
-          type="number"
+          type={biggerNumber ? "text" : "number"} inputMode="numeric" pattern="\d*" 
           min={2} step={1}
+          defaultValue={n?.toString()}
           onChange={onTargetChange}
           placeholder="n ≧ 2"
         />
       </label>
 
-      <p className="mt-3">Result:</p>
+      <p className="mt-3">
+        Result: <span className="text-gray-300 text-sm">({ dividerProgress })</span>
+      </p>
       <div>
         { result.map(i => `${i[0].toString()}${i[1] > 1 ? ` ^ ${i[1]}` : ""}`).join(' * ') }
         {!factorizationComplete && <LoaderCircle className="animate-spin inline-block ml-1" size={20} />}
